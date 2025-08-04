58db952a761499fcf8c1bf521859e20d042128fc
mixcoffee88 <jylee@ibank.co.kr>
2025-08-04 11:48:31 +0900

    .

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.core.config import settings
 
